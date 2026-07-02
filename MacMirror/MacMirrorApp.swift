7b47acd02ab9ad8d5cfc0e21d5e61fd40be095d9
Angel Velasquez <angelvelasquezdev@gmail.com>
2026-07-02 16:07:40 -0500

    Initial Commit

---
+++ MacMirror/MacMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMirrorApp.swift
+//  MacMirror
+//
+//  Created by Angel Velasquez on 2/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MacMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
